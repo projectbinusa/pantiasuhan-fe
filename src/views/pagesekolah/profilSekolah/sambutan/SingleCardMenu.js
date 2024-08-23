0dbd523c9f92ac1fe51c9b65ddcb7c0b5082a55c
din-ovita <dindanovitaputeriutami29@gmail.com>
2024-08-23 14:14:21 +0700

    navbar, home

--- src/views/pagesekolah/profilSekolah/sambutan/SingleCardMenu.js
+++ src/views/pagesekolah/profilSekolah/sambutan/SingleCardMenu.js
@@ -5,39 +5,6 @@ import axios from "axios";
 import { API_DUMMY } from "../../../../utils/base_URL";
 import "../../../../css/sambutan/sambutan.css"
 
-const announcements = [
-  {
-    id: 1,
-    title: "Pengumuman 1",
-    link: "/pengumuman1",
-    text: "Ini pengumuman pertama ni pengumuman pertama ni pengumuman pertama ni pengumuman pertamani pengumuman pertamani pengumuman pertamani pengumuman pertamani pengumuman pertama",
-  },
-  {
-    id: 2,
-    title: "Pengumuman 2",
-    link: "/pengumuman2",
-    text: "Ini pengumuman kedua",
-  },
-  {
-    id: 3,
-    title: "Pengumuman 3",
-    link: "/pengumuman3",
-    text: "Ini pengumuman ketiga",
-  },
-  {
-    id: 4,
-    title: "Pengumuman 4",
-    link: "/pengumuman4",
-    text: "Ini pengumuman keempat",
-  },
-  {
-    id: 5,
-    title: "Pengumuman 5",
-    link: "/pengumuman5",
-    text: "Ini pengumuman kelima",
-  },
-];
-
 const SingleCardMenu = () => {
   const [pengumuman, setPengumuman] = useState([]);
   const [sambutan, setSambutan] = useState([]);
@@ -71,10 +38,10 @@ const SingleCardMenu = () => {
   }, []);
 
   return (
-    <Grid container spacing={3} style={{ padding: "0 16px" }} direction="row">
-      <Grid item xs={12} sm={6} md={6} data-aos="fade-right"
+    <Grid container direction="row">
+      <Grid item xs={12} sm={12} md={6} data-aos="fade-right"
         data-aos-anchor-placement="bottom-bottom">
-        <Card style={{ maxWidth: 600 }}>
+        <Card>
           <CardContent style={{ padding: "0" }}>
             <div
               style={{
@@ -124,45 +91,14 @@ const SingleCardMenu = () => {
                   </div>
                 ))
               ) : (<></>)}
-              {/* 
-              {announcements.map((announcement, index) => (
-                <div key={announcement.id}>
-                  <Link
-                    href={announcement.link}
-                    style={{ color: "inherit", textDecoration: "none" }}
-                  >
-                    <Typography
-                      variant="h6"
-                      style={{
-                        marginBottom: "8px",
-                        fontWeight: "bold",
-                        fontSize: "1rem",
-                        fontFamily: "'Poppins', sans-serif" 
-                      }}
-                    >
-                      {announcement.text}
-                    </Typography>
-                  </Link>
-                  {index < announcements.length - 1 && (
-                    <hr
-                      style={{
-                        border: "none",
-                        borderBottom: "1px solid #000000",
-                        margin: "16px 0",
-                      }}
-                    />
-                  )}
-                </div>
-              ))}
-              */}
             </CardContent>
           </CardContent>
         </Card>
       </Grid>
 
-      <Grid item xs={12} sm={6} md={6} data-aos="fade-left"
+      <Grid item xs={12} sm={12} md={6} data-aos="fade-left"
         data-aos-anchor-placement="bottom-bottom">
-        <div style={{ maxWidth: 600, padding: "16px", borderRadius: "8px" }}>
+        <div className="berita-style">
           {sambutan.length > 0 ? (
             sambutan.map((sambutann) => (
               <img
@@ -193,21 +129,14 @@ const SingleCardMenu = () => {
             </Typography>
             {sambutan.length > 0 ? (
               sambutan.map((sambutann) => (
-                <Typography variant="body2" color="textSecondary" style={{ fontFamily: "'Poppins', sans-serif" }} className="content-sambutan">
-                  {sambutann.isi}
-                  <Link href="/sambutan"> ...Baca selengkapnya</Link>
-                </Typography>
+                <>
+                  <Typography variant="body2" color="textSecondary" style={{ fontFamily: "'Poppins', sans-serif" }} className="content-sambutan">
+                    {sambutann.isi}
+                  </Typography>
+                  <Link href="/sambutan" style={{ fontSize: "14px" }}>Baca selengkapnya</Link>
+                </>
               ))
             ) : (<></>)}
-
-            {/* <Typography variant="h6" component="div" style={{ fontWeight: "bold", fontFamily: "'Poppins', sans-serif"  }}>
-              Sambutan Kepala Sekolah
-            </Typography>
-            <Typography variant="body2" color="textSecondary" style={{fontFamily: "'Poppins', sans-serif" }}>
-              Assalamu'alaikum Warahmatullahi Wabarakatuh,
-              Puji syukur kita panjatkan kehadirat Allah SWT yang telah memberikan rahmat dan karunia-Nya sehingga kita dapat melaksanakan tugas dan aktivitas kita sehari-hari dengan baik.
-              <Link href="/sambutan"> ...Baca selengkapnya</Link>
-            </Typography> */}
           </div>
         </div>
       </Grid>
