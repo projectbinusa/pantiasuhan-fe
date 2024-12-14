331f8ebbf0dcb5a1d9ac3acd5cddf9cf34a969a6
fatiyasalsabila994 <117715933+fatiyasalsabila123@users.noreply.github.com>
2024-12-14 14:03:05 +0700

    Update base_URL.js

--- src/utils/base_URL.js
+++ src/utils/base_URL.js
@@ -1,8 +1,8 @@
- export const API_DUMMY = "http://localhost:2051";
-// export const API_DUMMY = "https://api-mccsemarang.ecomp.id";
+ // export const API_DUMMY = "http://localhost:2051";
+export const API_DUMMY = "https://api-mccsemarang.ecomp.id";
 // export const API_DUMMY_PYTHON = "https://api3-mccsemarang.ecomp.id";
 // export const API_DUMMY = "https://api.mccsemarang.com";
-// export const API_DUMMY_PYTHON = "https://api2.mccsemarang.com";
-export const API_DUMMY_PYTHON = "https://api.byrtagihan.com";
+export const API_DUMMY_PYTHON = "https://api2.mccsemarang.com";
+// export const API_DUMMY_PYTHON = "https://api.byrtagihan.com";
 // export const API_DUMMY_PYTHON = "http://127.0.0.1:2051";
 
