af5f30b1affbd241c2e28492c817b84da57ee781
fatiyasalsabila994 <117715933+fatiyasalsabila123@users.noreply.github.com>
2025-01-26 20:44:38 +0700

    Update base_URL.js

--- src/utils/base_URL.js
+++ src/utils/base_URL.js
@@ -1,6 +1,6 @@
-// export const API_DUMMY = "https://dev-api-panti.byrtagihan.com";
-export const API_DUMMY = "http://127.0.0.1:9982";
-export const API_DUMMY_SMART1 = "https://dev-api.byrtagihan.com";
-export const API_DUMMY_SMART = "http://127.0.0.1:9981";
+export const API_DUMMY = "https://dev-api-panti.byrtagihan.com";
+// export const API_DUMMY = "http://127.0.0.1:2051";
+export const API_DUMMY_SMART = "https://dev-api.byrtagihan.com";
+// export const API_DUMMY_SMART = "http://127.0.0.1:9981";
 export const API_DUMMY_ABSEN = "https://dev-api-absen.byrtagihan.com";
 export const API_DUMMY_BYRTGHN = "http://127.0.0.1:8891";
